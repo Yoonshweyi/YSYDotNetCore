234ec438b4dd23eaf9bf136c7715397e53c6c722
yoonshweyi2580 <yoonshweyi2580@gmail.com>
2024-01-19 09:39:12 +0630

    [Complete] notifliix,datatable,datetime picker

--- YSYDotNetCore.HTMLApp/custom.js
+++ YSYDotNetCore.HTMLApp/custom.js
@@ -1,22 +1,42 @@
 function successMessage(message){
-    Swal.fire({
-        title: "Success",
-        text: message,
-        icon: "success"
-      });
+   // Swal.fire({
+     //title: "Success",
+       // text: message,
+       // icon: "success"
+
+     // });
+     
+     Notiflix.Report.success(
+        'Success',
+        message,
+        'Okay',
+        );
 }
 
 function confirmMessage(message){
      return new Promise(function(myResolve, myReject) {
-        Swal.fire({
-            title: "Confirm",
-            text: "Are you sure want to delete?.",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "Yes",
+         Swal.fire({
+             title: "Confirm",
+             text: "Are you sure want to delete?.",
+             icon: "warning",
+             showCancelButton: true,
+             confirmButtonColor: "Yes",
             
-          }).then((result) => {
-            myResolve( result.isConfirmed); 
-          });
-        });
+           }).then((result) => {
+             myResolve( result.isConfirmed); 
+           });
+
+        // Notiflix.Confirm.show(
+        //   'Confirm',
+        //   message,
+        //   'Yes',
+        //   'No',
+        //   function okCb() {
+        //   myResolve(true);
+        //   },
+        //   function cancelCb() {
+        //   myResolve(false);
+        //   }
+        //  );
+         });
 }
